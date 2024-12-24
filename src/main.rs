14ba5d7c9054317a45ec0b67b3d89d4b18c6178f
Satoshi Warita <div9851.tech@gmail.com>
2024-12-24 18:53:45 +0000

    switch root directory

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,43 @@ use std::env;
 use std::ffi::CString;
 
 use nix::libc::SIGCHLD;
+use nix::mount::{mount, MsFlags};
 use nix::sched::{clone, CloneFlags};
 use nix::sys::wait::waitpid;
 use nix::unistd::execv;
 use nix::unistd::getpid;
+use nix::unistd::pivot_root;
 
 fn exec() -> isize {
     let args: Vec<CString> = env::args().map(|s| CString::new(s).unwrap()).collect();
+    let new_root = "/home/waritasa/mountpoint";
+    env::set_current_dir(new_root).unwrap();
+    println!("current directory: {:?}", env::current_dir().unwrap());
+    mount(
+        Some(""),
+        "/",
+        Some(""),
+        MsFlags::MS_SLAVE | MsFlags::MS_REC,
+        Some(""),
+    )
+    .unwrap();
+    mount(
+        Some("."),
+        ".",
+        Some(""),
+        MsFlags::MS_BIND | MsFlags::MS_REC,
+        Some(""),
+    )
+    .unwrap();
+    pivot_root(".", ".").unwrap();
+    mount(
+        Some("proc"),
+        "/proc",
+        Some("proc"),
+        MsFlags::empty(),
+        Some(""),
+    )
+    .unwrap();
     execv(&args[1], &args[1..]).unwrap();
     0
 }
@@ -21,7 +51,7 @@ fn main() {
         let pid = clone(
             Box::new(exec),
             &mut stack,
-            CloneFlags::empty(),
+            CloneFlags::CLONE_NEWNS | CloneFlags::CLONE_NEWPID,
             Some(SIGCHLD),
         )
         .unwrap();
